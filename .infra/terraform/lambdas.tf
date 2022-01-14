1ae98d7b7b582b5f8c0b3056bc980aeada704586
Rémy <remy.nts@gmail.com>
2022-01-14 20:57:21 +0100

    modify lambda rate

--- .infra/terraform/lambdas.tf
+++ .infra/terraform/lambdas.tf
@@ -4,6 +4,6 @@ module "lambda_streaming" {
   lambda_handler    = "stream.lambda_handler"
   dead_letter_queue = true
   memory_size       = 512
-  cron              = { "expression" = "rate(1 minute)", "input" : { "wallet_address" = "0x8e3cfca4995d2ec53e1001707cd6f3d58a32b334", "stream_type" = "transaction" } }
+  cron              = { "expression" = "rate(5 minute)", "input" : { "wallet_address" = "0x8e3cfca4995d2ec53e1001707cd6f3d58a32b334", "stream_type" = "transaction" } }
   env               = { "bsc_scan_api_key" : local.bsc_scan_api_key }
 }
